b58113162288d16ed9e358e4d2c1eab158669450
xlfdll <xlfdll@gmail.com>
2015-12-19 22:39:33 +0900

    Version 1.1: Latest Release

---
+++ FontViewer/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace FontViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.fontListBox = new System.Windows.Forms.ListBox();
+            this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.fontNameToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.fontFolderLinkToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.fontCountToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.copyrightToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchImageList = new System.Windows.Forms.ImageList(this.components);
+            this.searchComboBox = new System.Windows.Forms.ComboBox();
+            this.mainStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fontListBox
+            // 
+            resources.ApplyResources(this.fontListBox, "fontListBox");
+            this.fontListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.fontListBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.fontListBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.fontListBox.FormattingEnabled = true;
+            this.fontListBox.MultiColumn = true;
+            this.fontListBox.Name = "fontListBox";
+            this.fontListBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.fontListControl_DrawItem);
+            this.fontListBox.SelectedIndexChanged += new System.EventHandler(this.fontListBox_SelectedIndexChanged);
+            this.fontListBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fontListBox_KeyDown);
+            this.fontListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.fontListBox_MouseDoubleClick);
+            // 
+            // mainStatusStrip
+            // 
+            this.mainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontNameToolStripStatusLabel,
+            this.fontFolderLinkToolStripStatusLabel,
+            this.fontCountToolStripStatusLabel,
+            this.copyrightToolStripStatusLabel});
+            resources.ApplyResources(this.mainStatusStrip, "mainStatusStrip");
+            this.mainStatusStrip.Name = "mainStatusStrip";
+            this.mainStatusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            // 
+            // fontNameToolStripStatusLabel
+            // 
+            this.fontNameToolStripStatusLabel.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.fontNameToolStripStatusLabel.Name = "fontNameToolStripStatusLabel";
+            resources.ApplyResources(this.fontNameToolStripStatusLabel, "fontNameToolStripStatusLabel");
+            this.fontNameToolStripStatusLabel.Spring = true;
+            // 
+            // fontFolderLinkToolStripStatusLabel
+            // 
+            this.fontFolderLinkToolStripStatusLabel.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.fontFolderLinkToolStripStatusLabel.IsLink = true;
+            this.fontFolderLinkToolStripStatusLabel.Name = "fontFolderLinkToolStripStatusLabel";
+            resources.ApplyResources(this.fontFolderLinkToolStripStatusLabel, "fontFolderLinkToolStripStatusLabel");
+            this.fontFolderLinkToolStripStatusLabel.Click += new System.EventHandler(this.fontFolderLinkToolStripStatusLabel_Click);
+            // 
+            // fontCountToolStripStatusLabel
+            // 
+            this.fontCountToolStripStatusLabel.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.fontCountToolStripStatusLabel.Name = "fontCountToolStripStatusLabel";
+            resources.ApplyResources(this.fontCountToolStripStatusLabel, "fontCountToolStripStatusLabel");
+            // 
+            // copyrightToolStripStatusLabel
+            // 
+            this.copyrightToolStripStatusLabel.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.copyrightToolStripStatusLabel.Name = "copyrightToolStripStatusLabel";
+            resources.ApplyResources(this.copyrightToolStripStatusLabel, "copyrightToolStripStatusLabel");
+            // 
+            // searchButton
+            // 
+            resources.ApplyResources(this.searchButton, "searchButton");
+            this.searchButton.BackColor = System.Drawing.SystemColors.Window;
+            this.searchButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.GrayText;
+            this.searchButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.searchButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.searchButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.searchButton.ImageList = this.searchImageList;
+            this.searchButton.Name = "searchButton";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchImageList
+            // 
+            this.searchImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("searchImageList.ImageStream")));
+            this.searchImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.searchImageList.Images.SetKeyName(0, "Find.png");
+            this.searchImageList.Images.SetKeyName(1, "Clear.png");
+            // 
+            // searchComboBox
+            // 
+            resources.ApplyResources(this.searchComboBox, "searchComboBox");
+            this.searchComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.searchComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.searchComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.searchComboBox.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.searchComboBox.FormattingEnabled = true;
+            this.searchComboBox.Name = "searchComboBox";
+            this.searchComboBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.fontListControl_DrawItem);
+            this.searchComboBox.TextChanged += new System.EventHandler(this.searchComboBox_TextChanged);
+            this.searchComboBox.Enter += new System.EventHandler(this.searchComboBox_Enter);
+            this.searchComboBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchComboBox_KeyDown);
+            this.searchComboBox.Leave += new System.EventHandler(this.searchComboBox_Leave);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.searchComboBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.mainStatusStrip);
+            this.Controls.Add(this.fontListBox);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.mainStatusStrip.ResumeLayout(false);
+            this.mainStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fontListBox;
+        private System.Windows.Forms.StatusStrip mainStatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel copyrightToolStripStatusLabel;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ImageList searchImageList;
+        private System.Windows.Forms.ComboBox searchComboBox;
+        private System.Windows.Forms.ToolStripStatusLabel fontFolderLinkToolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel fontNameToolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel fontCountToolStripStatusLabel;
+    }
+}
+
---
+++ FontViewer/MainForm.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+using Microsoft.Win32;
+
+using FontViewer.Properties;
+
+namespace FontViewer
+{
+    public partial class MainForm : Form
+    {
+        #region Constructors
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event Methods
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            fontFolderLinkToolStripStatusLabel.Text = fontsFolderPath;
+
+            foreach (FontFamily font in fonts.Families)
+            {
+                fontListBox.Items.Add(font.Name);
+                searchComboBox.Items.Add(font.Name);
+            }
+
+            RegistryKey fontRegKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts");
+
+            foreach (String item in fontRegKey.GetValueNames())
+            {
+                fontsDictionory.Add(item, fontRegKey.GetValue(item).ToString());
+            }
+
+            fontCountToolStripStatusLabel.Text = String.Format(fontCountToolStripStatusLabel.Text, fontListBox.Items.Count.ToString());
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (searchButton.ImageIndex == 1)
+            {
+                searchComboBox.Text = String.Empty;
+                searchComboBox.Focus();
+            }
+        }
+
+        private void searchComboBox_Enter(object sender, EventArgs e)
+        {
+            if (String.Equals(searchComboBox.Text, Resources.EnterFontNameText))
+            {
+                // First erase string, then set formats.
+
+                searchComboBox.Text = String.Empty;
+                searchComboBox.Font = searchTextBoxRegularFont;
+                searchComboBox.ForeColor = SystemColors.WindowText;
+
+                searchComboBox.SelectedIndex = -1;
+            }
+        }
+
+        private void searchComboBox_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(searchComboBox.Text))
+            {
+                // First set formats, then fill in search tip string.
+
+                searchComboBox.Font = searchTextBoxItalicFont;
+                searchComboBox.ForeColor = SystemColors.GrayText;
+                searchComboBox.Text = Resources.EnterFontNameText;
+
+                searchButton.ImageIndex = 0;
+            }
+        }
+
+        private void searchComboBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(searchComboBox.Text))
+            {
+                // First search the font, then start third-party font viewer.
+
+                fontListBox.SelectedIndex = fontListBox.FindString(searchComboBox.Text);
+
+                fontListBox_KeyDown(sender, e);
+            }
+        }
+
+        private void searchComboBox_TextChanged(object sender, EventArgs e)
+        {
+            Boolean isSearchTextBoxEmpty = String.IsNullOrEmpty(searchComboBox.Text);
+
+            searchButton.ImageIndex = isSearchTextBoxEmpty ? 0 : 1;
+
+            if (!isSearchTextBoxEmpty)
+            {
+                fontListBox.SelectedIndex = fontListBox.FindString(searchComboBox.Text);
+            }
+        }
+
+        private void fontListControl_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Boolean isFontListBox = sender is ListBox;
+
+            String fontName = isFontListBox ? (sender as ListBox).Items[e.Index].ToString() : (sender as ComboBox).Items[e.Index].ToString();
+            Single fontPreviewSize = isFontListBox ? 25.0f : 11.5f;
+
+            drawFontItem(e, fontName, fontPreviewSize, isFontListBox);
+        }
+
+        private void fontListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (fontListBox.SelectedItem != null)
+            {
+                String fontName = fontListBox.SelectedItem.ToString();
+                String fontFileName = getFontFileName(fontName);
+
+                fontNameToolStripStatusLabel.Text = String.Format(@"{0} ({1})", fontName, fontFileName);
+            }
+        }
+
+        private void fontListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                start3rdFontViewer();
+            }
+        }
+
+        private void fontListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                start3rdFontViewer();
+            }
+        }
+
+        private void fontFolderLinkToolStripStatusLabel_Click(object sender, EventArgs e)
+        {
+            Process.Start(Path.Combine(Environment.SystemDirectory, "rundll32.exe"), "shell32.dll,SHHelpShortcuts_RunDLL FontsFolder");
+
+            fontFolderLinkToolStripStatusLabel.LinkVisited = true;
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private void drawFontItem(DrawItemEventArgs e, String fontName, Single fontPreviewSize, Boolean drawHeadFoot)
+        {
+            using (FontFamily font = new FontFamily(fontName))
+            {
+                e.DrawBackground();
+
+                if (e.State == DrawItemState.Selected)
+                {
+                    e.Graphics.FillRectangle(Brushes.LightBlue, e.Bounds);
+                }
+
+                using (StringFormat fontStringFormat = new StringFormat())
+                {
+                    fontStringFormat.Alignment = StringAlignment.Center;
+                    fontStringFormat.LineAlignment = StringAlignment.Center;
+
+                    Brush stringBrush = (Enum.Format(typeof(DrawItemState), e.State, "g").Contains("Selected")) ? Brushes.White : Brushes.Black;
+
+                    e.Graphics.DrawRectangle(Pens.LightBlue, e.Bounds);
+
+                    if (drawHeadFoot)
+                    {
+                        String fontFileName = getFontFileName(font);
+                        String fontBottomName = Path.Combine(fontsFolderPath, fontFileName);
+
+                        e.Graphics.DrawString(fontName, SystemFonts.MenuFont, stringBrush, e.Bounds.Left + 10, e.Bounds.Top + 10);
+                        e.Graphics.DrawString(fontBottomName, SystemFonts.MenuFont, stringBrush, e.Bounds.Left + 10, e.Bounds.Bottom - 25);
+                    }
+
+                    // Use try-catch structure to handle "cannot draw with specified fonts" error.
+
+                    try { e.Graphics.DrawString(fontName, new Font(font, fontPreviewSize, getFontStyle(font)), stringBrush, e.Bounds, fontStringFormat); }
+                    catch { }
+
+                    e.Graphics.Dispose();
+                }
+            }
+        }
+
+        private FontStyle getFontStyle(FontFamily fontFamily)
+        {
+            if (fontFamily.IsStyleAvailable(FontStyle.Regular))
+            {
+                return FontStyle.Regular;
+            }
+
+            if (fontFamily.IsStyleAvailable(FontStyle.Bold))
+            {
+                return FontStyle.Bold;
+            }
+
+            if (fontFamily.IsStyleAvailable(FontStyle.Italic))
+            {
+                return FontStyle.Italic;
+            }
+
+            if (fontFamily.IsStyleAvailable(FontStyle.Strikeout))
+            {
+                return FontStyle.Strikeout;
+            }
+
+            if (fontFamily.IsStyleAvailable(FontStyle.Underline))
+            {
+                return FontStyle.Underline;
+            }
+
+            return FontStyle.Regular;
+        }
+
+        private String getFontFileName(String fontName)
+        {
+            FontFamily font = new FontFamily(fontName);
+
+            return getFontFileName(font);
+        }
+
+        private String getFontFileName(FontFamily fontFamily)
+        {
+            String fontRegName = fontFamily.GetName(CultureInfo.GetCultureInfo("en-us").LCID);
+
+            foreach (KeyValuePair<String, String> item in fontsDictionory)
+            {
+                if (item.Key.Contains(fontRegName))
+                {
+                    return item.Value;
+                }
+            }
+
+            return String.Empty;
+        }
+
+        private void start3rdFontViewer()
+        {
+            if (fontListBox.SelectedIndex >= 0)
+            {
+                String fontName = fontListBox.SelectedItem.ToString();
+                String fontFileName = getFontFileName(fontName);
+
+                if (!String.IsNullOrEmpty(fontFileName))
+                {
+                    String command = Path.Combine(fontsFolderPath, fontFileName);
+
+                    Process.Start(command);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        private static readonly Font searchTextBoxItalicFont = new Font(SystemFonts.MenuFont, FontStyle.Italic);
+        private static readonly Font searchTextBoxRegularFont = new Font(SystemFonts.MenuFont, FontStyle.Regular);
+
+        private static readonly InstalledFontCollection fonts = new InstalledFontCollection();
+        private static readonly Dictionary<String, String> fontsDictionory = new Dictionary<String, String>();
+
+        private static readonly String fontsFolderPath = Path.Combine(Environment.GetEnvironmentVariable("SystemRoot"), "Fonts");
+
+        #endregion
+    }
+}
